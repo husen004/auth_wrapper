2050041d6fb5270208f4eb72b2d2e081d2dd7764
husen ikary <husenikary@mail.ru>
2025-09-21 14:42:45 +0300

    clear

--- Backend/internal/auth/middleware.go
+++
@@ -1,6 +0,0 @@
-package auth
-
-// This file is intentionally left empty as the middleware functionality
-// has been included in the handlers.go file as part of the Handler struct.
-//
-// The AuthMiddleware method is implemented in handlers.go
--- Backend/internal/auth/models.go
+++
@@ -1,7 +0,0 @@
-package auth
-
-// This file is intentionally left empty as the model structures
-// have been included in the handlers.go file.
-//
-// The User, UserResponse, LoginRequest, RegisterRequest, TokenResponse,
-// RefreshRequest, and Claims structs are all defined in handlers.go
